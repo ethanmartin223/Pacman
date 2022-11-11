a173adc631a11b0ae3f85a89ca98de36f07b36ae
ethanmartin223 <ethan_l_martin@etownstudents.org>
2022-11-11 00:14:21 -0500

    Updated Code

--- core/src/com/lethan/pacman/WallSpriteLookup.java
+++ core/src/com/lethan/pacman/WallSpriteLookup.java
@@ -31,11 +31,65 @@ public enum WallSpriteLookup {
             {1, 1, 1}
     },"corner_top_right"),
 
-    CORNER_TOP_LEFT(new int[][]{
+    DOUBLE_WALL_CORNER_BOTTOM_LEFT(new int[][]{
+            {0, 1, 0},
             {0, 1, 1},
-            {1, 1, 1},
-            {1, 1, 1}
-    },"corner_top_left"),
+            {0, 0, 0}
+    },"double_wall_corner_bottom_left"),
+
+    DOUBLE_WALL_CORNER_BOTTOM_RIGHT(new int[][]{
+            {0, 1, 0},
+            {1, 1, 0},
+            {0, 0, 0}
+    },"double_wall_corner_bottom_right"),
+
+    DOUBLE_WALL_CORNER_TOP_RIGHT(new int[][]{
+        {0, 0, 0},
+        {1, 1, 0},
+        {0, 1, 0}
+    },"double_wall_corner_top_right"),
+
+    DOUBLE_WALL_CORNER_TOP_LEFT(new int[][]{
+        {0, 0, 0},
+        {0, 1, 1},
+        {0, 1, 0}
+    },"double_wall_corner_top_left"),
+
+    DOUBLE_WALL_END_LEFT(new int[][]{
+        {2, 0, 0},
+        {1, 1, 0},
+        {2, 0, 0}
+    },"double_wall_end_left"),
+
+    DOUBLE_WALL_END_RIGHT(new int[][]{
+        {0, 0, 2},
+        {0, 1, 1},
+        {0, 0, 2}
+    },"double_wall_end_right"),
+
+    HORIZONTAL_DOUBLE_WALL(new int[][]{
+        {2, 0, 2},
+        {1, 1, 1},
+        {2, 0, 2}
+    },"horizontal_double_wall"),
+
+    VERTICAL_DOUBLE_WALL(new int[][]{
+        {2, 1, 2},
+        {0, 1, 0},
+        {2, 1, 2}
+    },"vertical_double_wall"),
+
+    VERTICAL_WALL_RIGHT(new int[][]{
+        {2, 1, 1},
+        {0, 1, 1},
+        {2, 1, 1}
+    },"vertical_wall_right"),
+
+    VERTICAL_WALL_LEFT(new int[][]{
+            {1, 1, 2},
+            {1, 1, 0},
+            {1, 1, 2}
+    },"vertical_wall_left");
 
 
     private final int[][] layout;
